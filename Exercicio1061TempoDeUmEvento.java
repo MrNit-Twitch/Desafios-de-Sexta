b61fd965337e1adf0d24e8d9b03d2ff41885d193
Mr.Nit <contato.mr.nit@gmail.com>
2024-08-30 16:14:24 -0300

    Atualização de arquivos 30/08/2024

--- Exercicio1061TempoDeUmEvento.java
+++ Exercicio1061TempoDeUmEvento.java
@@ -5,25 +5,23 @@ import java.util.Scanner;
 public class Exercicio1061TempoDeUmEvento {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Dia ");
-        String iniDay = scanner.nextLine();
+        String[] iniDay = scanner.nextLine().split("\\s");
         String[] iniTime = scanner.nextLine().split(" : ");
-        System.out.print("Dia ");
-        String finDay = scanner.nextLine();
+        String[] finDay = scanner.nextLine().split("\\s");
         String[] finTime = scanner.nextLine().split(" : ");
 
-        int iniDayInSec = Integer.parseInt(iniDay) * 86400;
-        int finDayInSec = Integer.parseInt(finDay) * 86400;
+        int iniDayInSec = Integer.parseInt(iniDay[1]) * 86400;
+        int finDayInSec = Integer.parseInt(finDay[1]) * 86400;
         int iniHourInSec = Integer.parseInt(iniTime[0]) * 3600;
         int finHourInSec = Integer.parseInt(finTime[0]) * 3600;
         int iniMinInSec = Integer.parseInt(iniTime[1]) * 60;
         int finMinInSec = Integer.parseInt(finTime[1]) * 60;
         int iniSec = Integer.parseInt(iniTime[2]);
         int finSec = Integer.parseInt(finTime[2]);
-        int totDay = 0;
-        int totHour = 0;
-        int totMin = 0;
-        int totSec = 0;
+        int totDay;
+        int totHour;
+        int totMin;
+        int totSec;
 
         totDay = (finDayInSec - iniDayInSec) / 86400;
         if (finHourInSec > iniHourInSec) {
@@ -36,7 +34,7 @@ public class Exercicio1061TempoDeUmEvento {
         if (finMinInSec > iniMinInSec) {
             totMin = (finMinInSec - iniMinInSec) / 60;
         } else {
-            totMin = (3600 - (finMinInSec - finMinInSec)) / 60;
+            totMin = (3600 - (finMinInSec - iniMinInSec)) / 60;
             totHour = totHour - 1;
         }
 
@@ -47,9 +45,9 @@ public class Exercicio1061TempoDeUmEvento {
             totMin = totMin - 1;
         }
 
-        System.out.println(totDay + " dia (s)");
-        System.out.println(totHour + " hora (s)");
-        System.out.println(totMin + " minuto (s)");
-        System.out.println(totSec + " segundo (s)");
+        System.out.println(totDay + " dia(s)");
+        System.out.println(totHour + " hora(s)");
+        System.out.println(totMin + " minuto(s)");
+        System.out.println(totSec + " segundo(s)");
     }
 }
